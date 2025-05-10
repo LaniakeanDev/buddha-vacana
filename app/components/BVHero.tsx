cff22a3ad5be1400ba3b847b746b53d67b91e300
LaniakeanDev <laniakean.dev@gmail.com>
2025-05-10 10:34:17 +0200

    color fixes

--- app/components/BVHero.tsx
+++ app/components/BVHero.tsx
@@ -14,14 +14,14 @@ export default function BVHero() {
       <BVSearchbar className="absolute top-4 right-4" />
       <div className="relative z-10 ml-36 pt-32 w-4/5">
         <div className="p-4 w-[472px]">
-          <p className="text-justify mb-4">
+          <p className="text-justify text-black mb-4">
             Il se peut que certains d'entre vous se disent : “C'en est fini de la parole de l'Enseignant. Nous n'avons
             plus d'Enseignant.” Mais ils ne devraient pas penser cela, car ce que j'ai déclaré et fait connaître comme
             le <span className="font-bold text-bvprimary-green">Dhamma</span> et le{' '}
             <span className="font-bold text-bvprimary-green">Vinaya</span> sera votre enseignant une fois que je serai
             parti.
           </p>
-          <p className="text-justify mb-4">
+          <p className="text-justify text-black mb-4">
             siyā kho panānanda, tumhākaṃ evamassa ‘atītasatthukaṃ pāvacanaṃ, natthi no satthā’ti. na kho panetaṃ,
             ānanda, evaṃ daṭṭhabbaṃ. yo vo, ānanda, mayā dhammo ca vinayo ca desito paññatto, so vo mamaccayena satthā.
           </p>
