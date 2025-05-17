254573f7dc7a6e8980284df6e55ab092fe8e01ab
LaniakeanDev <laniakean.dev@gmail.com>
2025-05-17 11:01:59 +0200

    themes complete with toggle

--- app/components/header/BVHeader.tsx
+++ app/components/header/BVHeader.tsx
@@ -1,9 +1,11 @@
 import { BVSVGAsset } from '../BVAsset';
 import BVLogo from '@/public/assets/svg/bv-logo.svg';
+import ThemeToggle from '../BVThemeToggle';
 
 export default function BVHeader() {
   return (
-    <header className="relative w-full h-48 bg-bvdark-green grid place-items-center">
+    <header className="relative w-full h-48 bg-default grid place-items-center">
+      <ThemeToggle className="absolute top-4 right-4" />
       <div className="absolute top-0 left-0 h-full pl-4 grid place-items-center">
         <BVSVGAsset
           SVGAsset={BVLogo}
@@ -13,7 +15,7 @@ export default function BVHeader() {
       </div>
       <h1>
         <p className="text-6xl text-gold font-bold">Buddha Vacana</p>
-        <p className="text-[42px] text-white font-bold">La parole du Bouddha</p>
+        <p className="text-[42px] text-white font-bold text-default">La parole du Bouddha</p>
       </h1>
     </header>
   );
