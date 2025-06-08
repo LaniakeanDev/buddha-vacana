129dce52bf4ac1d3cbfa893de88a11c3d08aaa34
LaniakeanDev <laniakean.dev@gmail.com>
2025-06-08 23:30:55 +0200

    pretty much complete

--- app/components/BVHero.tsx
+++ app/components/BVHero.tsx
@@ -5,17 +5,25 @@ import DiceIcon from '@/public/assets/svg/hero/random.svg';
 
 export default function BVHero() {
   return (
-    <section className="relative w-full h-[820px] 2xs:!h-[850px] xs:!h-[800px] md:h-[750px] lg:h-[700px]">
+    <section className="relative w-full h-[820px] 2xs:!h-[850px] xs:!h-[800px] md:!h-[750px] lg:!h-[700px]">
       <Image
-        className="object-cover z-0"
+        className="object-cover z-0 hidden md:!block"
         src="/assets/img/hero-bg.jpg"
+        alt="Arbre entouré de nature"
+        sizes="100vw"
+        fill
+      />
+      <Image
+        className="object-cover z-0 block md:!hidden"
+        src="/assets/img/hero-bg-sm.jpg"
         alt="Arbre entouré de nature"
         sizes="100vw"
         fill
       />
       <BVSearchbar className="absolute top-4 right-4" />
       <div className="relative w-[95%] xs:!w-[90%] z-10 m-auto md:ml-10 xl:ml-36 pt-16 sm:w-4/5 flex flex-col items-center md:items-start">
-        <div className="xs:!mt-4 sm:mt-0 p-4 2xs:!p-8 max-w-[472px] backdrop-blur-xs bg-[#fcf6ce]/10 xs:!mb-8 rounded-xl">
+        {/* <div className="xs:!mt-4 sm:mt-0 p-4 2xs:!p-8 max-w-[472px] backdrop-blur-xs bg-[#fcf6ce]/60 md:!bg-[#fcf6ce]/40 xl:!bg-[#fcf6ce]/20 2xl:!bg-[#fcf6ce]/0 mb-2 xs:!mb-8 rounded-xl"> */}
+        <div className="xs:!mt-4 sm:mt-0 !p-4 2xs:!p-8 max-w-[472px] backdrop-blur-xs bg-gradient-to-b from-[#fcf6ce]/60 to-[#feedaf]/60 xs:!to-[#ffedb8]/60 xl:!bg-[#fcf6ce]/20 2xl:!bg-[#fcf6ce]/0 mb-8 rounded-xl">
           <p className="text-justify !text-(--bvdarktext) mb-4">
             Il se peut que certains d'entre vous se disent : “C'en est fini de la parole de l'Enseignant. Nous n'avons
             plus d'Enseignant.” Mais ils ne devraient pas penser cela, car ce que j'ai déclaré et fait connaître comme
@@ -34,21 +42,21 @@ export default function BVHero() {
         <div className="w-full grid  place-items-center max-w-[472px]">
           <div className="flex flex-row gap-8 justify-center">
             <button className="primary-btn w-32 xs:!w-48">
-              <p className="!text-(--bvwhite)">Dhamma</p>
+              <p className="!text-(--bvwhite) text-lg xs:!text-2xl">Dhamma</p>
             </button>
             <button className="primary-btn w-32 xs:!w-48">
-              <p className="!text-(--bvwhite)">Vinaya</p>
+              <p className="!text-(--bvwhite) text-lg xs:!text-2xl">Vinaya</p>
             </button>
           </div>
         </div>
       </div>
       <div className="w-full grid place-items-center mt-8 sm:mt-16 md:mt-0 md:block">
-        <div className="w-fit flex flex-row gap-4 md:absolute md:bottom-4 md:right-10 xl:right-[20vw] 3xl:right-[30vw] z-10">
-          <button className="flex flex-col items-center bg-black/25 3xl:bg-black/35 rounded-4xl p-4 w-32 h-32">
+        <div className="w-fit flex flex-row gap-4 md:absolute md:bottom-4 md:right-10 xl:right-[10vw] 2xl:right-[15vw] 3xl:right-[20vw] z-10">
+          <button className="flex flex-col items-center bg-black/35 3xl:bg-black/35 rounded-2xl p-4 w-32 h-32">
             <NewContentIcon />
             <p className="!text-(--bvwhite) mt-2">Nouveautés</p>
           </button>
-          <button className="flex flex-col items-center bg-black/25 rounded-4xl p-4 w-32 h-32">
+          <button className="flex flex-col items-center bg-black/35 rounded-2xl p-4 w-32 h-32">
             <DiceIcon />
             <p className="!text-(--bvwhite) mt-2">Au hasard</p>
           </button>
--- app/components/header/BVHeader.tsx
+++ app/components/header/BVHeader.tsx
@@ -6,11 +6,11 @@ export default function BVHeader() {
   return (
     <header className="relative w-full h-30 sm:h-36 md:h-40 lg:h-48 bg-default grid place-items-center">
       <ThemeToggle className="absolute top-2 right-2 sm:top-4 sm:right-4" />
-      <div className="absolute top-0 left-0 h-full xs:!pl-2 sm:pl-4 grid place-items-center">
+      <div className="absolute top-0 left-0 h-full xs:!pl-2 sm:!pl-4 grid place-items-center">
         <BVSVGAsset
           SVGAsset={BVLogo}
           alt="Logo représentant une roue du Dhamma dans un lotus"
-          containerClassName="w-20 h-20 xs:!w-28 xs:!h-28 sm:w-32 sm:h-32"
+          containerClassName="w-20 h-20 xs:!w-28 xs:!h-28 sm:!w-32 sm:!h-32"
         />
       </div>
       <h1 className="translate-x-8 xs:!translate-x-12 lg:translate-x-0">
@@ -19,7 +19,7 @@ export default function BVHeader() {
           Buddha Vacana
         </p>
         {/* <p className="sm:text-[25.25px] md:text-[33.75px] lg:text-[42px] font-bold  !text-(--maintitle-fr)">La parole du Bouddha</p> */}
-        <p className="text-[17px] 2xs:!text-[20px] xs:!text-[25.25px] sm:!text-[33.75px] lg:!text-[42px] font-bold  !text-(--maintitle-fr)">
+        <p className="text-[16.75px] 2xs:!text-[21px] xs:!text-[25.25px] sm:!text-[33.75px] lg:!text-[42px] font-bold  !text-(--maintitle-fr)">
           La parole du Bouddha
         </p>
       </h1>
