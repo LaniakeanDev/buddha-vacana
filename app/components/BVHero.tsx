0fff6a4686650443e6b394c237784a05912992b4
LaniakeanDev <laniakean.dev@gmail.com>
2025-06-14 20:21:33 +0200

    renamed color variables

--- app/components/BVHero.tsx
+++ app/components/BVHero.tsx
@@ -25,30 +25,30 @@ export default function BVHero() {
       <div className="relative w-[95%] xs:!w-[90%] z-10 m-auto md:ml-10 xl:ml-36 pt-16 sm:w-4/5 flex flex-col items-center md:items-start">
         {/* <div className="xs:!mt-4 sm:mt-0 p-4 2xs:!p-8 max-w-[472px] backdrop-blur-xs bg-[#fcf6ce]/60 md:!bg-[#fcf6ce]/40 xl:!bg-[#fcf6ce]/20 2xl:!bg-[#fcf6ce]/0 mb-2 xs:!mb-8 rounded-xl"> */}
         <div className="xs:!mt-4 sm:mt-0 !p-4 2xs:!p-8 max-w-[472px] backdrop-blur-xs bg-gradient-to-b from-[#fcf6ce]/60 to-[#feedaf]/60 xs:!to-[#ffedb8]/60 xl:!bg-[#fcf6ce]/20 2xl:!bg-[#fcf6ce]/0 mb-8 rounded-xl">
-          <p className="text-justify !text-(--bvdarktext) mb-4">
+          <p className="text-justify !text-(--darktext) mb-4">
             Il se peut que certains d'entre vous se disent : “C'en est fini de la parole de l'Enseignant. Nous n'avons
             plus d'Enseignant.” Mais ils ne devraient pas penser cela, car ce que j'ai déclaré et fait connaître comme
-            le <span className="font-bold !text-(--bvprimary-green)">Dhamma</span> et le{' '}
-            <span className="font-bold !text-(--bvprimary-green)">Vinaya</span> sera votre enseignant une fois que je
+            le <span className="font-bold !text-(--primary-green)">Dhamma</span> et le{' '}
+            <span className="font-bold !text-(--primary-green)">Vinaya</span> sera votre enseignant une fois que je
             serai parti.
           </p>
-          <p className="text-justify !text-(--bvdarktext) mb-4">
+          <p className="text-justify !text-(--darktext) mb-4">
             siyā kho panānanda, tumhākaṃ evamassa ‘atītasatthukaṃ pāvacanaṃ, natthi no satthā’ti. na kho panetaṃ,
             ānanda, evaṃ daṭṭhabbaṃ. yo vo, ānanda, mayā dhammo ca vinayo ca desito paññatto, so vo mamaccayena satthā.
           </p>
-          <p className="w-full text-center !text-(--bvdarktext)">
-            — <span className="font-bold !text-(--bvprimary-green)">Mahāparinibbāna Sutta</span> —
+          <p className="w-full text-center !text-(--darktext)">
+            — <span className="font-bold !text-(--primary-green)">Mahāparinibbāna Sutta</span> —
           </p>
         </div>
         <div className="w-full grid  place-items-center max-w-[472px]">
           <div className="flex flex-row gap-8 justify-center">
             <Link href="/dhamma">
               <button className="primary-btn w-32 xs:!w-48">
-                <p className="!text-(--bvwhite) text-lg xs:!text-2xl">Dhamma</p>
+                <p className="!text-(--white) text-lg xs:!text-2xl">Dhamma</p>
               </button>
             </Link>
             <button className="primary-btn w-32 xs:!w-48">
-              <p className="!text-(--bvwhite) text-lg xs:!text-2xl">Vinaya</p>
+              <p className="!text-(--white) text-lg xs:!text-2xl">Vinaya</p>
             </button>
           </div>
         </div>
@@ -57,11 +57,11 @@ export default function BVHero() {
         <div className="w-fit flex flex-row gap-4 md:absolute md:bottom-4 md:right-10 xl:right-[10vw] 2xl:right-[15vw] 3xl:right-[20vw] z-10">
           <button className="flex flex-col items-center bg-black/35 3xl:bg-black/35 rounded-2xl p-4 w-32 h-32">
             <NewContentIcon />
-            <p className="!text-(--bvwhite) mt-2">Nouveautés</p>
+            <p className="!text-(--white) mt-2">Nouveautés</p>
           </button>
           <button className="flex flex-col items-center bg-black/35 rounded-2xl p-4 w-32 h-32">
             <DiceIcon />
-            <p className="!text-(--bvwhite) mt-2">Au hasard</p>
+            <p className="!text-(--white) mt-2">Au hasard</p>
           </button>
         </div>
       </div>
--- app/dhamma/components/NikayaCard.tsx
+++ app/dhamma/components/NikayaCard.tsx
@@ -2,11 +2,11 @@ export default function NikayaCard({ titlePl, titleFr, description, count, short
   return (
     <div className="rounded-xl card relative pt-6 pb-4 px-6 sm:!px-8 w-[90vw] max-w-xl min-h-[310px] cursor-pointer">
       <div className="absolute bottom-2 left-0 w-12 h-10 grid place-items-center bg-(--emphasis) rounded-tr-md rounded-br-md">
-        <p className="font-bold">{short}</p>
+        <p className="font-bold !text-white">{short}</p>
       </div>
       <div className="w-full grid place-items-center mb-4">
         <h3>
-          <p className="text-xl sm:!text-2xl text-center !text-(--gold)">{titlePl}</p>
+          <p className="text-xl sm:!text-2xl text-center !text-(--card-title)">{titlePl}</p>
           <p className="text-lg sm:!text-xl text-center">{titleFr}</p>
         </h3>
       </div>
