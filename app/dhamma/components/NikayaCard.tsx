ede5e6b1c3fc03fcf0a4a3d301d739e0483dcb23
LaniakeanDev <laniakean.dev@gmail.com>
2025-06-15 16:59:35 +0200

    bunch of fixes

--- app/dhamma/components/NikayaCard.tsx
+++ app/dhamma/components/NikayaCard.tsx
@@ -4,8 +4,8 @@ type NikayaCardProps = Omit<INikayaPresentationData, 'id'>;
 
 export default function NikayaCard({ plTitle, frTitle, description, count, short }: NikayaCardProps) {
   return (
-    <div className="card min-h-[310px]">
-      {/* <div className="card rounded-xl relative cursor-pointer pt-6 pb-4 px-6 sm:!px-8 w-[90vw] max-w-xl min-h-[310px]"> */}
+    // <div className="card min-h-[310px]">
+    <div className="card rounded-xl relative cursor-pointer pt-6 pb-4 px-6 sm:!px-8 w-[90vw] max-w-xl min-h-[310px]">
       <div className="absolute bottom-2 left-0 w-12 h-10 grid place-items-center bg-(--emphasis) rounded-tr-md rounded-br-md">
         <p className="font-bold !text-white">{short}</p>
       </div>
