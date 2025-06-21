395637071f5ff32505b19382b5077cca8b0d6561
LaniakeanDev <laniakean.dev@gmail.com>
2025-06-21 14:46:46 +0200

    nice-icons

--- app/components/SuttaCard.tsx
+++ app/components/SuttaCard.tsx
@@ -1,5 +1,9 @@
 import CardContent from './CardContent';
 import CardIdentifier from './CardIdentifier';
+import HourglassIcon from '@/public/assets/svg/hourglass.svg';
+import HourglassIconLightEnv from '@/public/assets/svg/hourglass-lightenv.svg';
+import TranslatorIcon from '@/public/assets/svg/sutta/translator.svg';
+import TranslatorIconLightEnv from '@/public/assets/svg/sutta/translator-lightenv.svg';
 
 export default function SuttaCard({
   plTitle,
@@ -17,8 +21,16 @@ export default function SuttaCard({
       <CardIdentifier content={identifier} className="!w-16" />
       <CardContent plTitle={plTitle} frTitle={frTitle} description={description} />
       <div className="w-full flex flex-row justify-between">
-        <p>{translator}</p>
-        <p>{readingTime}</p>
+        <div className="flex flex-row items-center gap-2">
+          <TranslatorIcon className="w-5 h-5 translate-y-[-2px] show-on-dark" />
+          <TranslatorIconLightEnv className="w-5 h-5 translate-y-[-2px] hide-on-dark" />
+          <p className="text-sm">{translator}</p>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <HourglassIcon className="w-5 h-5 show-on-dark" />
+          <HourglassIconLightEnv className="w-5 h-5 hide-on-dark" />
+          <p className="text-sm">{readingTime} mn</p>
+        </div>
       </div>
     </div>
   );
