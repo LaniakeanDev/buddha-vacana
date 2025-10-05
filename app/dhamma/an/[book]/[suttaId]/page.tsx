b896a8ced15965b81d3152199585513bd685fc24
LaniakeanDev <laniakean.dev@gmail.com>
2025-10-05 16:21:16 +0200

    AN sutta works

--- app/dhamma/an/[book]/[suttaId]/page.tsx
+++ app/dhamma/an/[book]/[suttaId]/page.tsx
@@ -6,37 +6,37 @@ import GraciousFail from '@/app/components/GraciousFail';
 
 interface AnguttaraSuttaPageProps {
   params: {
-    bookNumber: number;
+    book: number;
     suttaId: number;
   };
 }
 
 export default async function AnguttaraSuttaPage({ params }: AnguttaraSuttaPageProps) {
-  const { bookNumber, suttaId } = await params;
-  if (!Number.isInteger(bookNumber) || !Number.isInteger(suttaId) || bookNumber < 1 || bookNumber > 12 || suttaId < 1) {
+  const { book, suttaId } = await params;
+  if (!Number.isInteger(+book) || !Number.isInteger(+suttaId) || +book < 1 || +book > 12 || +suttaId < 1) {
     return <GraciousFail message="La ressource que vous demandez n'existe pas" />;
   }
   try {
-    const suttaData = getSuttaData('anguttara', String(bookNumber), String(suttaId));
+    const suttaData = getSuttaData('an', book, String(suttaId));
     if (!suttaData) {
-      throw new Error(`File containing data for AN ${bookNumber}.${suttaId} unavailable`);
+      throw new Error(`File containing data for AN ${book}.${suttaId} unavailable`);
     }
     if (!isISuttaData(suttaData)) {
-      throw new Error(`Data structure in AN ${bookNumber}.${suttaId} doesn't match ISuttaCardData interface`);
+      throw new Error(`Data structure in AN ${book}.${suttaId} doesn't match ISuttaCardData interface`);
     }
     if (suttaData && isISuttaData(suttaData)) {
       return (
         <>
           <p className="breadcrumbs">
-            <Link href="/dhamma">Dhamma</Link> {' > '} <Link href="/dhamma/anguttara">AN</Link>
-            {' > '} <Link href="/dhamma/anguttara">AN {bookNumber}</Link> {' > '} AN {bookNumber}.{suttaId}
+            <Link href="/dhamma">Dhamma</Link> {' > '} <Link href="/dhamma/an">AN</Link>
+            {' > '} <Link href={`/dhamma/an${book}`}>AN {book}</Link> {' > '} AN {book}.{suttaId}
           </p>
           <SuttaPageContent suttaData={suttaData} />
         </>
       );
     }
   } catch (error) {
-    console.error(`Failed to load sutta data from anguttara/${bookNumber}/${suttaId}:`, error);
+    console.error(`Failed to load sutta data from anguttara/${book}/${suttaId}:`, error);
     return <GraciousFail message="Soutta non trouvé" />;
   }
 }
